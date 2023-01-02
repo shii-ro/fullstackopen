a70cb9ff47cbfdd7a851836b92ece841f6f5e12b
shii-ro <wesley.7.castro@gmail.com>
2023-01-02 04:34:36 -0300

    step 4.2

---
+++ part4/bloglist/controllers/blogs.js
@@ -0,0 +1,25 @@
+const blogsRouter = require('express').Router();
+const Blog = require('../models/blog');
+
+//Routes
+blogsRouter.get('/', (req, res) => {
+    Blog
+        .find({})
+        .then(blogs => {
+            res.json(blogs);
+        })
+        .catch(error => next(error))
+})
+
+blogsRouter.post('/', (req, res) => {
+    const blog = new Blog(req.body);
+
+    blog
+        .save()
+        .then(result => {
+            res.status(201).json(result);
+        })
+})
+
+
+module.exports = blogsRouter;
