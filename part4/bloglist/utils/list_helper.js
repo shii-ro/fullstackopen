dc0d26260c1a464b0aa360dfd0b9aea1efdb0584
shii-ro <wesley.7.castro@gmail.com>
2023-01-07 13:56:43 -0300

    4.4

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,5 +1,8 @@
-const dummy = (blogs) => {
-    return 1;
+const dummy = (blogs) => 1;
+
+const totalLikes = (blogs) => {
+  const result = blogs.map((blog) => blog.likes).reduce((a, b) => a + b);
+  return result;
 };
 
-module.exports = { dummy };
+module.exports = { dummy, totalLikes };
